53f45a08efb8e9272288e03b2c8f6ce1a0eb85f2
Igor Loskutov <igor.loskutoff@gmail.com>
2023-06-24 21:32:41 +0700

    init

---
+++ packages/astroblog/packages/astroblog/src/pages/index.astro.mjs
@@ -0,0 +1,8 @@
+export { renderers } from '../../../../../../../renderers.mjs';
+export { onRequest } from '../../../../../../../_empty-middleware.mjs';
+import '../../../../../../../chunks/astro.90fe6bbf.mjs';
+import 'html-escaper';
+
+const page = () => import('../../../../../../../chunks/pages/index.astro.67c9d762.mjs');
+
+export { page };
---
+++ packages/astroplugin/README.md
@@ -0,0 +1,11 @@
+# astroplugin
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build astroplugin` to build the library.
+
+## Running unit tests
+
+Run `nx test astroplugin` to execute the unit tests via [Jest](https://jestjs.io).
